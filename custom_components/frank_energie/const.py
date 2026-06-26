ae58c1e67564ea7dd4b0254ffe13a4f1c7edad2b
Roy <hidiho@gmail.com>
2026-06-27 00:25:13 +0200

    Update VERSION to 2026.6.27

--- custom_components/frank_energie/const.py
+++ custom_components/frank_energie/const.py
@@ -33,7 +33,7 @@ _LOGGER: logging.Logger = logging.getLogger(__name__)
 
 # --- Domain Information ---
 DOMAIN: Final[str] = "frank_energie"
-VERSION: Final[str] = "2026.6.21"
+VERSION: Final[str] = "2026.6.27"
 ATTRIBUTION: Final[str] = "Data provided by Frank Energie"
 UNIQUE_ID: Final[str] = "frank_energie"
 TIMEZONE_AMSTERDAM: Final[str] = "Europe/Amsterdam"
